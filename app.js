9924fe5896c3acdff1d7210d646491cdfcd942fe
moha9 <hisocaa0@gmail.com>
2022-08-10 09:37:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
+// const express = require('express');
+// const app = express();
+
+// app.get('/test', (req, res)=>{
+//     // localhost:port/endpoint
+//     // localhost:3000/test
+//     res.send('GET requets and this is the response');
+// });
+
+// app.listen(3000, ()=>{
+//     console.log('SERVER ARE WORKING ...');
+// });
 const express = require('express');
+
 const app = express();
 
-app.get('/test', (req, res)=>{
-    // localhost:port/endpoint
-    // localhost:3000/test
-    res.send('GET requets and this is the response');
+app.use(express.json());
+
+const db = [
+  {fName:"user1",favFood:"food1"},
+  {fName:"user1",favFood:"food1"},
+  {fName:"user1",favFood:"food1"},
+  {fName:"user1",favFood:"food1"},
+  {fName:"user1",favFood:"food1"},
+];
+
+app.get('/week5', (req, res)=>{
+    res.send('welcome to week 5');
 });
 
-app.listen(3000, ()=>{
-    console.log('SERVER ARE WORKING ...');
+app.listen(4000, ()=>{
+    console.log(`SERVER ON localhost:4000`);
 });
